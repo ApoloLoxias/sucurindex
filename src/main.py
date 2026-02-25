8c646785dd3fe3d7ccc4362247c7ab70f2982e46
ApoloLoxias <apololoxias@gmail.com>
2026-02-25 08:15:17 -0300

    print_file_entry (to console)

--- src/main.py
+++ src/main.py
@@ -9,15 +9,8 @@ from src.parsing import *
 
 def main():
     print("Starting manual test for SuccurIndex")
-    meta = read_file_metadata("uv.lock")
-    file_entry = FileEntry(
-        path = "uv.lock",
-        size = meta["size"]
-    )
-
-    burn_toml_file_entry(file_entry)
-
-    print(str(read_toml_file_entry(str(file_entry.id))))
+    file_entry = read_toml_file_entry("7c9f4a03-b75f-4b14-bcb7-389208b3aab0")
+    print_file_entry(file_entry, ["name", "links"])
 
     print("Test concluded")
 
--- src/output.py
+++ src/output.py
@@ -14,3 +14,31 @@ def burn_toml_file_entry(file_entry: FileEntry) -> str:
         return f"Successfully wrote '{file_entry.name}'/{file_entry.id} FileEntry to '{file_path}'"
     except Exception as e:
         return f"Failed to write '{file_entry.name}'/{file_entry.id} FileEntry to '{file_path}'"
+
+def print_file_entry(file_entry: FileEntry, tags: list[str]=None) -> str:
+    output = []
+    invalid_tags = []
+    valid_tags = [
+            "name",
+            "id",
+            "description",
+            "tags",
+            "links",
+            "mtime",
+            "size",
+        ]
+
+    if not tags:
+        print(file_entry)
+        return f"Printed FileEntry '{file_entry.name}'/{file_entry.id} to console"
+
+    invalid_tags = [t for t in tags if t not in valid_tags]
+    if invalid_tags != []:
+        print(f"Invalid tag(s): {invalid_tags}, valid tags include {valid_tags}")
+        return f"Invalid tag(s): {invalid_tags}"
+
+    output = [f"{t}: {getattr(file_entry, t)}" for t in tags]
+
+    for line in output:
+        print(line)
+    return f"Printed FileEntry '{file_entry.name}'/{file_entry.id}'s {tags} to console"
